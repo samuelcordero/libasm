04941e453e6a60e29f6b8a8ff51bc37a34b5f9a6
Samuel Cordero <samuelcorderorodrigo@gmail.com>
2024-09-24 16:03:50 +0200

    added errno handling for syscalls

--- srcs/ft_read.s
+++ srcs/ft_read.s
@@ -1,7 +1,20 @@
 section .text
     global ft_read
+    extern __errno_location ; for setting errno
 
 ft_read:
-    mov     rax, 0         ; set rax to 0 (for read syscall)
-    syscall                ; syscall to read
+    mov     rax, 0          ; set rax to 0 (for read syscall)
+    syscall                 ; syscall to read
+    test    rax, rax
+    js      .syserror       ; if negative handle error
+    ret
+
+.syserror:
+    neg     rax
+    push    rbx
+    mov     rbx, rax
+    call    __errno_location
+    mov     [rax], rbx
+    mov     rax, -1
+    pop     rbx
     ret
--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,7 +1,20 @@
 section .text
     global ft_write
+    extern __errno_location ; errno for syscall failure
 
 ft_write:
-    mov     rax, 1         ; set rax to 1 (for write syscall)
-    syscall                ; syscall to write
+    mov     rax, 1          ; set rax to 1 (for write syscall)
+    syscall                 ; syscall to write
+    test    rax, rax
+    js      .syserror       ; if negative handle error
+    ret
+
+.syserror:
+    neg     rax
+    push    rbx
+    mov     rbx, rax
+    call    __errno_location
+    mov     [rax], rbx
+    mov     rax, -1
+    pop     rbx
     ret
